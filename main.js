5d327147820b2e1613e07c40cf89491d2706845b
baharma <bagusaditya1899@gmail.com>
2024-02-04 03:57:06 +0800

    story

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import Aos from 'aos'
 import 'aos/dist/aos.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import './src/js/script'
 
 
 Aos.init();

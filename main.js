966347d2d5e2b917536acad3e984abb73bb8824a
Baharma <bagusaditya1899@gmail.com>
2024-02-03 22:50:23 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import './src/css/main.css'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+
+Aos.init();
